fe049e4ed41de0d0cb15bdf5b31131cbf9f73173
Dinis Cruz <dinis.cruz@owasp.org>
2025-10-13 16:05:38 +0100

    formatting changs

--- tests/unit/fast_api/routes/test_Routes__Cache.py
+++ tests/unit/fast_api/routes/test_Routes__Cache.py
@@ -1,81 +1,15 @@
-import pytest
 from unittest                                                                           import TestCase
-from mgraph_ai_service_cache.fast_api.Cache_Service__Fast_API                           import Cache_Service__Fast_API
-from mgraph_ai_service_cache.service.cache.Cache__Config                                import Cache__Config
-from mgraph_ai_service_cache.service.cache.Cache__Service                               import Cache__Service
-from mgraph_ai_service_cache_client.client_contract.Service__Fast_API__Client           import Service__Fast_API__Client
-from mgraph_ai_service_cache_client.client_contract.Service__Fast_API__Client__Config   import Service__Fast_API__Client__Config
-from mgraph_ai_service_cache_client.schemas.cache.enums.Enum__Cache__Storage_Mode       import Enum__Cache__Storage_Mode
-from osbot_fast_api.utils.Fast_API_Server                                               import Fast_API_Server
-from osbot_fast_api_serverless.fast_api.Serverless__Fast_API__Config                    import Serverless__Fast_API__Config
-from osbot_utils.helpers.duration.decorators.capture_duration                           import capture_duration
 from osbot_utils.testing.__                                                             import __
-from osbot_utils.type_safe.primitives.core.Safe_UInt                                    import Safe_UInt
-from osbot_utils.utils.Dev import pprint
-from osbot_utils.utils.Env import in_github_action
+from osbot_utils.utils.Env                                                              import in_github_action
 from osbot_utils.utils.Objects                                                          import obj
-
 from mgraph_ai_service_mitmproxy.fast_api.routes.Routes__Cache import Routes__Cache
 from mgraph_ai_service_mitmproxy.service.cache.Proxy__Cache__Service                    import Proxy__Cache__Service
-from mgraph_ai_service_mitmproxy.service.cache.schemas.Schema__Cache__Config            import Schema__Cache__Config
-from mgraph_ai_service_mitmproxy.service.cache.schemas.Schema__Cache__Stats             import Schema__Cache__Stats
 
-# todo: refactor most of these methods into the cache_service tests, since they are not testing Routes__Cache methods
 class test_Routes__Cache(TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:                                                                # Setup cache service and test infrastructure
-        # pytest.skip("write tests to use setup__testing__cache_service__with__test_data and to actually hit the Routes__Cache")
-        # with capture_duration() as duration:
-        #     cache_config = Cache__Config(storage_mode=Enum__Cache__Storage_Mode.MEMORY)         # Setup cache service backend
-        #     cls.serverless_config = Serverless__Fast_API__Config(enable_api_key=False)
-        #     cls.cache_service__fast_api = Cache_Service__Fast_API(config=cls.serverless_config,
-        #                                                           cache_service=Cache__Service(cache_config=cache_config))
-        #
-        #     cls.fast_api_server = Fast_API_Server(app=cls.cache_service__fast_api.app())
-        #     cls.server_url      = cls.fast_api_server.url().rstrip("/")
-        #
-        #     cls.cache_service__fast_api.setup()
-        #     cls.fast_api_server.start()
-        #
-        #     cls.client_config = Service__Fast_API__Client__Config(base_url=cls.server_url)          # Setup cache client
-        #     cls.cache_client = Service__Fast_API__Client(config=cls.client_config)
-        #
-        #     cls.cache_config = Schema__Cache__Config(enabled    = True               ,              # Setup proxy cache service
-        #                                              base_url   = cls.server_url     ,
-        #                                              namespace  = "test-cache-routes",
-        #                                              timeout    = 2                  )
-        #
-        #     cls.cache_service = Proxy__Cache__Service(cache_client=cls.cache_client,
-        #                                               cache_config=cls.cache_config,
-        #                                               stats=Schema__Cache__Stats())
-        #
-        #     cls._populate_test_cache()                                                              # Populate test data
-        #
-        # assert duration.seconds < 1.5
         cls.routes_cache = Routes__Cache()
-
-    # @classmethod
-    # def tearDownClass(cls) -> None:
-    #     cls.fast_api_server.stop()
-
-    # @classmethod
-    # def _populate_test_cache(cls):                                              # Populate cache with test data
-    #     test_urls = [ "https://example.com/"                ,                   # Add some test pages with transformations
-    #                   "https://docs.diniscruz/ai"           ,
-    #                   "https://docs.diniscruz/ai/about.html"]
-    #
-    #     for url in test_urls:
-    #         cls.cache_service.store_transformation(target_url   = url               ,       # Store HTML transformation
-    #                                                wcf_command  = "url-to-html"     ,
-    #                                                content      = f"<html><body><h1>Test content for {url}</h1></body></html>",
-    #                                                metadata     = {"status_code": 200, "content_type": "text/html"})
-    #
-    #         # Store text transformation
-    #         cls.cache_service.store_transformation(target_url   = url                                               ,
-    #                                                wcf_command  = "url-to-lines"                                    ,
-    #                                                content      = f"Test text content for {url}"                    ,
-    #                                                metadata     = {"status_code": 200, "content_type": "text/plain"})
 
     def test_setUpClass(self):
         with self.routes_cache as _:
