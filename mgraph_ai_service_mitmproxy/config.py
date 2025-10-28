b43f48519109d26da148776861e05dd8281e64c4
Dinis Cruz <dinis.cruz@owasp.org>
2025-10-28 14:19:34 +0000

    fixed version of mgraph_ai_service_cache_client

--- mgraph_ai_service_mitmproxy/config.py
+++ mgraph_ai_service_mitmproxy/config.py
@@ -3,5 +3,5 @@ from mgraph_ai_service_mitmproxy import package_name
 SERVICE_NAME                             = package_name
 FAST_API__TITLE                          = "MGraph AI Service mitmproxy"
 FAST_API__DESCRIPTION                    = "Base template for MGraph-AI microservices"
-LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['mgraph_ai_service_cache_client==0.7.0' ,
+LAMBDA_DEPENDENCIES__FAST_API_SERVERLESS = ['mgraph_ai_service_cache_client==0.8.0' ,
                                             'osbot-fast-api-serverless==1.26.0'     ]
